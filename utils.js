aa2664debfd4aed633ae07f6450a470164591760
Maxime Moreillon <m.moreillon@gmail.com>
2023-10-23 12:29:17 +0900

    minor

--- utils.js
+++ utils.js
@@ -1,2 +1,2 @@
 exports.get_id_of_item = (item) =>
-  item._id || item.properties._id || item.identity.low || item.identity
+  item._id || item.properties?._id || item.identity?.low || item.identity
